c97aeeca95f664582be0c9143c840d5164855d48
mohinieassy <mohini.m@eassyserve.in>
2023-12-05 23:49:07 +0530

    localstorage and DSA question to print the number on odd indexes

---
+++ excersises/51223_localstorage&datetime/index.js
@@ -0,0 +1,3 @@
+// 9.1: Create two variables myHobby , age . Now set their value in local storage (according to your hobby and age).
+
+// After setting also get value from local storage and display their values in console.
